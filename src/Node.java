0c2f0af1e6a22f8acea9c73fe9cf0f79707927bc
w4ng-cpu <y.a.wang@lancaster.ac.uk>
2023-02-16 10:05:20 +0000

    starting node

--- src/Node.java
+++ src/Node.java
@@ -2,10 +2,14 @@ package src;
 
 import src.generator.ProgramSearcher;
 
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
 
 
-public class Main {
+public class MainNode {
     public static void main(String args[]) {
         ProgramSearcher generateSource = new ProgramSearcher(1, 2);
         String result = generateSource.startSearch();
